e87df262d83766c1411af086b4d486293b186254
Joy <yoiselrexrgp@gmail.com>
2024-02-02 21:29:35 -0500

    Casi completojuju

--- src/components/StyleList.jsx
+++ src/components/StyleList.jsx
@@ -24,6 +24,41 @@ export function StyleList() {
         name={"????"}
         price={5}
       />
+      <Styles
+        img={
+          "https://th.bing.com/th?id=OIP.Q1x7I1qKb_8BjNpQfzGE9gHaHJ&w=254&h=245&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+        }
+        name={"????"}
+        price={5}
+      />
+      <Styles
+        img={
+          "https://th.bing.com/th?id=OIP.B0CJpbWlGDfBr8tt2elVxwHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+        }
+        name={"Corte"}
+        price={"200"}
+      />
+      <Styles
+        img={
+          "https://th.bing.com/th?id=OIP.Pv_EONYO260HTt1CM9a_BgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+        }
+        name={"Cortecito barato"}
+        price={"5000"}
+      />
+      <Styles
+        img={
+          "https://th.bing.com/th?id=OIP.Q1x7I1qKb_8BjNpQfzGE9gHaHJ&w=254&h=245&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+        }
+        name={"????"}
+        price={5}
+      />
+      <Styles
+        img={
+          "https://th.bing.com/th?id=OIP.Q1x7I1qKb_8BjNpQfzGE9gHaHJ&w=254&h=245&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+        }
+        name={"????"}
+        price={5}
+      />
     </section>
   );
 }
--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -2,7 +2,13 @@ import "./Styles.css";
 export function Styles({ img, alt, name, price }) {
   return (
     <div className="styles">
-      <img src={img} alt={alt} />
+      <img
+        src={img}
+        alt={alt}
+        onClick={() => {
+          alert("mmwebo");
+        }}
+      />
       <p>{name}</p>
       <span>Precio: ${price}</span>
     </div>
