e0e8fd5ba10b46b658c2e6e7a1071d93efc66b51
Joy <yoiselrexrgp@gmail.com>
2024-02-02 23:26:44 -0500

    Cards modificadas

--- src/components/StyleList.jsx
+++ src/components/StyleList.jsx
@@ -5,59 +5,94 @@ export function StyleList() {
     <section className="stylelist">
       <Styles
         img={
-          "https://th.bing.com/th?id=OIP.B0CJpbWlGDfBr8tt2elVxwHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+          "https://i.pinimg.com/564x/59/5c/ad/595cad4cd223f91cf1ca71b7f69de7c5.jpg"
         }
-        name={"Corte"}
+        name={"Burst Fade"}
         price={"200"}
       />
       <Styles
         img={
-          "https://th.bing.com/th?id=OIP.Pv_EONYO260HTt1CM9a_BgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+          "https://i.pinimg.com/564x/9c/75/f5/9c75f5e708cbbde7ce9483fc031f4397.jpg"
         }
-        name={"Cortecito barato"}
-        price={"5000"}
-      />
-      <Styles
-        img={
-          "https://th.bing.com/th?id=OIP.Q1x7I1qKb_8BjNpQfzGE9gHaHJ&w=254&h=245&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
-        }
-        name={"????"}
-        price={5}
-      />
-      <Styles
-        img={
-          "https://th.bing.com/th?id=OIP.Q1x7I1qKb_8BjNpQfzGE9gHaHJ&w=254&h=245&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
-        }
-        name={"????"}
-        price={5}
-      />
-      <Styles
-        img={
-          "https://th.bing.com/th?id=OIP.B0CJpbWlGDfBr8tt2elVxwHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
-        }
-        name={"Corte"}
+        name={"Low Fade"}
         price={"200"}
       />
       <Styles
         img={
-          "https://th.bing.com/th?id=OIP.Pv_EONYO260HTt1CM9a_BgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+          "https://i.pinimg.com/564x/83/15/06/831506f6f0022f97ca3a945058bf66ad.jpg"
         }
-        name={"Cortecito barato"}
-        price={"5000"}
+        name={"Mid Fade"}
+        price={"200"}
       />
       <Styles
         img={
-          "https://th.bing.com/th?id=OIP.Q1x7I1qKb_8BjNpQfzGE9gHaHJ&w=254&h=245&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+          "https://i.pinimg.com/564x/d2/0e/61/d20e61fbf923ff02621f787334cee625.jpg"
         }
-        name={"????"}
-        price={5}
+        name={"High Fade"}
+        price={"200"}
       />
       <Styles
         img={
-          "https://th.bing.com/th?id=OIP.Q1x7I1qKb_8BjNpQfzGE9gHaHJ&w=254&h=245&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+          "https://i.pinimg.com/564x/74/4f/23/744f2366dc7bda06dbfa2653ea03ed27.jpg"
         }
-        name={"????"}
-        price={5}
+        name={"Táper Fade"}
+        price={"200"}
+      />
+      <Styles
+        img={
+          "https://i.pinimg.com/564x/bc/24/10/bc2410b14f6df51059fe44f95d930bd1.jpg"
+        }
+        name={"Mullet Fade"}
+        price={"200"}
+      />
+      <Styles
+        img={
+          "https://i.pinimg.com/564x/ca/6b/5c/ca6b5cd5d8f3716ad8a59f38ddbcd1a5.jpg"
+        }
+        name={"Skins Fade"}
+        price={"200"}
+      />
+      <Styles
+        img={
+          "https://i.pinimg.com/564x/e8/05/a2/e805a200fe16834670c7127c745c946b.jpg"
+        }
+        name={"Corte Clasico"}
+        price={"150"}
+      />
+      <Styles
+        img={
+          "https://i.pinimg.com/564x/06/5f/db/065fdb6d98ffe4e4d573954a71ee0172.jpg"
+        }
+        name={"Mohicano"}
+        price={"200"}
+      />
+      <Styles
+        img={
+          "https://i.pinimg.com/736x/b3/aa/f8/b3aaf830d3ee2bd9fedaece58611a180.jpg"
+        }
+        name={"Cortes con diseño"}
+        price={"250"}
+      />
+      <Styles
+        img={
+          "https://i.pinimg.com/564x/46/8a/e8/468ae8b5c6b472d39824e631dea71f94.jpg"
+        }
+        name={"Cortes con diseño"}
+        price={"250"}
+      />
+      <Styles
+        img={
+          "https://i.pinimg.com/564x/97/23/df/9723df859137a1cb5ab451f7847d845c.jpg"
+        }
+        name={"Cortes con diseño"}
+        price={"250"}
+      />
+      <Styles
+        img={
+          "https://i.pinimg.com/564x/c1/f5/7e/c1f57e8f7b80a8bd6d6d120e47a113f0.jpg"
+        }
+        name={"Rapado haircut"}
+        price={"100"}
       />
     </section>
   );
