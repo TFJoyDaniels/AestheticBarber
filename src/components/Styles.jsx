7b676ab1938c72939041849522f249e46c0617eb
Joy <yoiselrexrpg@gmail.com>
2024-03-18 12:15:36 -0400

    modal-disabled

--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -1,15 +1,11 @@
 import "./Styles/Styles.css";
-import { useState } from "react";
-import { Modal } from "./Modal";
 
 // eslint-disable-next-line react/prop-types
 export function Styles({ img, alt, name, price }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="styles-container">
-      <Modal isOpen={isModalOpen} closeModal={() => setIsModalOpen(false)} />
       <div className="styles">
-        <img onClick={() => setIsModalOpen(true)} src={img} alt={alt} />
+        <img src={img} alt={alt} />
         <div className="styles-info">
           <p>{name}</p>
           <span>Precio: ${price}</span>
