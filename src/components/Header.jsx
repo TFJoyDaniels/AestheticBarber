25ace29fa48e7e3e3b34f5a1216fac4587264a7d
Joy <yoiselrexrpg@gmail.com>
2024-03-18 11:49:36 -0400

    added bg and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+// /* eslint-disable react/jsx-no-target-blank */
 import "./Styles/Header.css";
+
 export function Header() {
   return (
-    <header className="header">
-      <h1>Aesthetic Barber Shop</h1>
+    <header>
+      <div className="header-container">
+        <h1>Bienvenid@ a Aesthetic Barber</h1>
+        <a
+          target="_blank"
+          href="https://api.whatsapp.com/send/?phone=%2B5350046951&text&type=phone_number&app_absent=0"
+          rel="noreferrer"
+        >
+          Reserva Ya
+        </a>
+        <p>Explora nuestra tienda</p>
+      </div>
     </header>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import "./Styles/Nav.css";
+export function Nav() {
+  return (
+    <nav>
+      <div className="logo"></div>
+      <div className="menu-icon"></div>
+    </nav>
+  );
+}
